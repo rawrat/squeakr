982c7f4cb003907d649818ff00bb382e7cfa3cee
Angelo Laub <angelolaub@gmail.com>
2019-01-29 15:05:45 +0100

    add frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
