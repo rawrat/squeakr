82533a193bd14b7601e2861e4c647c46f9a428cd
Angelo Laub <angelolaub@gmail.com>
2019-02-12 19:17:23 +0100

    enable contractpays: true

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -14,5 +14,6 @@ module.exports = {
     httpEndpoint: "http://127.0.0.1:8888",
     chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
     brokerUrl: 'http://127.0.0.1:4401',
+    contractpays: true,
   }
 }
